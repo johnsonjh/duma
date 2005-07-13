2bd37eac99ed3bebe3a60f633aa5377786b8e3f8
Hayati Ayguen <h_ayguen@web.de>
2005-07-13 02:08:19 +0000

    header and C again

--- print.h
+++ print.h
@@ -21,7 +21,7 @@
  *
  *
  * FILE CONTENTS:
- * internal implementation file
+ * internal header file
  * contains aborting, printing functions with minor system/platform dependencies
  */
 
@@ -34,234 +34,17 @@
  * debugger should not re-enter malloc(), so stdio is out.
  */
 
-
-/* define prototypes / forward declarations */
-
-static int  sprintNumber(char* obuffer, ef_number number, ef_number base);
-static int  EF_sprintf(char* buffer, const char *pattern, va_list args);
-static void EF_Abort(const char * pattern, ...);
-static void EF_Print(const char * pattern, ...);
-static void EF_Exit(const char * pattern, ...);
-
-
-/*
- * NUMBER_BUFFER_SIZE is the longest character string that could be needed
- * to represent an unsigned integer, assuming we might print in base 2.
- */
-#define  NUMBER_BUFFER_SIZE  (sizeof(ef_number) * NBBY)
-
-#define STRING_BUFFER_SIZE  1024
-
-
-/*
- * internal function to print a number into a buffer
- * int sprintNumber(char* obuffer, ef_number number, ef_number base)
- */
-static int
-sprintNumber(char* obuffer, ef_number number, ef_number base)
-{
-  char   buffer[NUMBER_BUFFER_SIZE+1];
-  char * s = &buffer[NUMBER_BUFFER_SIZE];
-  int    size;
-  ef_number  digit;
-
-  do
-  {
-    if ( --s == buffer )
-      EF_Abort("\nElectric Fence: Internal error printing number.");
-
-    digit = number % base;
-    *s = (char)( (digit < 10) ? ('0' + digit) : ('a' + digit -10) );
-
-  } while ( (number /= base) > 0 );
-
-  size = &buffer[NUMBER_BUFFER_SIZE] - s;
-  buffer[NUMBER_BUFFER_SIZE] = '\0';
-  strcpy(obuffer, s);
-  return size;
-}
-
-
-
-/*
- * internal function to print a formatted string into a buffer
- * int sprintf(char* buffer, const char *pattern, va_list args)
- */
-static int
-EF_sprintf(char* buffer, const char *pattern, va_list args)
-{
-  char    c;
-  static const char  bad_pattern[] = "\nElectric Fence: Bad pattern specifier %%%c in EF_Print().\n";
-  const char *  s = pattern;
-  int len = 0;
-  ef_number n;
-
-  c = *s++;
-  while ( c )
-  {
-    if ( c == '%' )
-    {
-      c = *s++;
-      switch ( c ) {
-      case '%':
-        buffer[len++] = c;
-        break;
-      case 'a':
-        /*
-         * Print an address passed as a void pointer.
-         * The type of ef_number must be set so that
-         * it is large enough to contain all of the
-         * bits of a void pointer.
-         */
-        n = (ef_number) va_arg(args, void *);
-        len += sprintNumber(&buffer[len], n, 0x10);
-        break;
-      case 's':
-        {
-          const char *  string;
-          size_t    length;
-
-          string = va_arg(args, char *);
-          if (string)
-          {
-            length = strlen(string);
-            strcpy(&buffer[len], string);
-          }
-          else
-          {
-            length = 4; /* = strlen("NULL") */
-            strcpy(&buffer[len], "NULL");
-          }
-          len += length;
-        }
-        break;
-      case 'd':
-        {
-          int  n = va_arg(args, int);
-          if ( n < 0 )
-          {
-            buffer[len++] = '-';
-            n = -n;
-          }
-          len += sprintNumber(&buffer[len], n, 10);
-        }
-        break;
-      case 'x':
-        n = (ef_number) va_arg(args, u_int);
-        len += sprintNumber(&buffer[len], n, 0x10);
-        break;
-      case 'c':
-        /* characters are passed as int ! */
-        buffer[len++] = (char)va_arg(args, int);
-        break;
-      default:
-        EF_Print(bad_pattern, c);
-      }
-    }
-    else
-      buffer[len++] = c;
-    
-    c = *s++;
-  }
-
-  buffer[len] = '\0';
-  return len;
-}
-
-
-
-/*
- * external abort function
- * on Visual C++ it additionally prints to Debug Output of the IDE
- * void EF_Abort(const char * pattern, ...)
- */
-static void
-EF_Abort(const char * pattern, ...)
-{
-  char buffer[STRING_BUFFER_SIZE];
-  int lena, lenb;
-  va_list  args;
-  va_start(args, pattern);
-
-  strcpy(buffer, "\nElectricFence Aborting: ");
-  lena = strlen(buffer);
-  lenb = EF_sprintf(&buffer[lena], pattern, args);
-  strcat(buffer, "\n");
-#ifdef _MSC_VER
-  _RPT0(_CRT_WARN, buffer);
+#ifdef __cplusplus
+extern "C" {
 #endif
-  write(2, buffer, lena + lenb + 1);
-  va_end(args);
 
-#ifndef WIN32
-  /*
-   * I use kill(getpid(), SIGILL) instead of abort() because some
-   * mis-guided implementations of abort() flush stdio, which can
-   * cause malloc() or free() to be called.
-   */
-  kill(getpid(), SIGILL);
-#else
-  /* Windows doesn't have a kill() */
-  abort();
+void EF_Abort(const char * pattern, ...);
+void EF_Print(const char * pattern, ...);
+void EF_Exit(const char * pattern, ...);
+
+#ifdef __cplusplus
+} /* extern "C" */
 #endif
-  /* Just in case something handles SIGILL and returns, exit here. */
-  _exit(-1);
-}
 
-
-
-/*
- * external print function
- * on Visual C++ it additionally prints to Debug Output of the IDE
- * void EF_Print(const char * pattern, ...)
- */
-static void
-EF_Print(const char * pattern, ...)
-{
-  char buffer[STRING_BUFFER_SIZE];
-  int len;
-  va_list  args;
-  va_start(args, pattern);
-
-  len = EF_sprintf(buffer, pattern, args);
-#ifdef _MSC_VER
-  _RPT0(_CRT_WARN, buffer);
-#endif
-  write(2, buffer, len);
-  va_end(args);
-}
-
-
-
-/*
- * external exit function
- * on Visual C++ it additionally prints to Debug Output of the IDE
- * void EF_Exit(const char * pattern, ...)
- */
-static void
-EF_Exit(const char * pattern, ...)
-{
-  char buffer[STRING_BUFFER_SIZE];
-  int lena, lenb;
-  va_list  args;
-  va_start(args, pattern);
-
-  strcpy(buffer, "\nElectricFence Exiting: ");
-  lena = strlen(buffer);
-  lenb = EF_sprintf(&buffer[lena], pattern, args);
-  strcat(buffer, "\n");
-#ifdef _MSC_VER
-  _RPT0(_CRT_WARN, buffer);
-#endif
-  write(2, buffer, lena + lenb + 1);
-  va_end(args);
-
-  /*
-   * I use _exit() because the regular exit() flushes stdio,
-   * which may cause malloc() or free() to be called.
-   */
-  _exit(-1);
-}
-
-
+  
 #endif /* EF_PRINT_H */
---
+++ sem_inc.c
@@ -0,0 +1,167 @@
+
+/*
+ * Electric Fence - Red-Zone memory allocator.
+ * Copyright (C) 2002-2005 Hayati Ayguen <h_ayguen@web.de>, Procitec GmbH
+ * License: GNU LGPL (GNU Lesser General Public License, see COPYING-GPL)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * FILE CONTENTS:
+ * internal implementation file
+ * contains thread safety functions (semaphore lock/release)
+ */
+
+#include "sem_inc.h"
+
+#ifndef EF_NO_THREAD_SAFETY
+
+#ifndef WIN32
+  #include <pthread.h>
+  #include <semaphore.h>
+#else
+  #define WIN32_LEAN_AND_MEAN 1
+  #include <windows.h>
+  #include <winbase.h>
+#endif
+
+
+/*
+ * EF_sem is a semaphore used to allow one thread at a time into
+ * these routines.
+ * Also, we use semInited as a boolean to see if we should be
+ * using the semaphore.
+ * semThread is set to the thread id of the thread that currently
+ * has the semaphore so that when/if it tries to get the semaphore
+ * again (realloc calling malloc/free) - nothing will happen to the
+ * semaphore.
+ * semDepth is used to keep track of how many times the same thread
+ * gets the semaphore - so we know when it is actually freed.
+ */
+
+
+
+#ifndef WIN32
+static sem_t      EF_sem = { 0 };
+static pthread_t  semThread = (pthread_t) 0;
+
+#else /* WIN32 */
+static SECURITY_ATTRIBUTES  semSecAttr;
+
+#ifndef UNICODE
+#define SEM_NAME_TYPE char
+#define SEM_STRCPY    strcpy
+#define SEM_STRCAT    strcat
+static char       semObjectName[] = "EFence_";
+#else
+#define SEM_NAME_TYPE wchar_t
+#define SEM_STRCPY    wcscpy
+#define SEM_STRCAT    wcscat
+static wchar_t    semObjectName[] = L"EFence_";
+#endif
+
+static DWORD      semThread = 0;
+static HANDLE     semHandle = 0;
+#endif
+
+static int        semInited = 0;
+static int        semDepth  = 0;
+
+
+static void EF_init_sem(void)
+{
+#ifndef WIN32
+  if (sem_init(&EF_sem, 0, 1) >= 0)
+    semInited = 1;
+#else
+  SEM_NAME_TYPE   semLocalName[32];
+  SEM_NAME_TYPE   acPID[16];
+  DWORD pid;
+
+  pid = GetCurrentProcessId();
+  SEM_STRCPY(semLocalName, semObjectName);
+  /* append ProcessId() to get inter-process unique semaphore name */
+  acPID[0] = 'A' + (SEM_NAME_TYPE)( (pid >> 28) & 0x0F );
+  acPID[1] = 'A' + (SEM_NAME_TYPE)( (pid >> 24) & 0x0F );
+  acPID[2] = 'A' + (SEM_NAME_TYPE)( (pid >> 20) & 0x0F );
+  acPID[3] = 'A' + (SEM_NAME_TYPE)( (pid >> 16) & 0x0F );
+  acPID[4] = 'A' + (SEM_NAME_TYPE)( (pid >> 12) & 0x0F );
+  acPID[5] = 'A' + (SEM_NAME_TYPE)( (pid >>  8) & 0x0F );
+  acPID[6] = 'A' + (SEM_NAME_TYPE)( (pid >>  4) & 0x0F );
+  acPID[7] = 'A' + (SEM_NAME_TYPE)( (pid      ) & 0x0F );
+  acPID[8] = 0;
+  SEM_STRCAT( semLocalName, acPID );
+
+  semSecAttr.nLength = sizeof(semSecAttr);
+  semSecAttr.lpSecurityDescriptor = NULL;
+  semSecAttr.bInheritHandle = FALSE;
+
+  semHandle = CreateSemaphore( &semSecAttr      /* pointer to security attributes */
+                               , 1              /* initial count */
+                               , 10             /* maximum count */
+                               , semLocalName   /* pointer to semaphore-object name */
+                             );
+  semInited = 1;
+#endif
+}
+
+
+void EF_get_sem(void)
+{
+  if (!semInited)     EF_init_sem();    /* initialize if necessary */
+  if (!semInited)     EF_Abort("\nCouldn't initialise semaphore");
+
+#ifndef WIN32
+  if ( pthread_self() != semThread )
+  {
+    while (sem_wait(&EF_sem) < 0) ;   /* wait for the semaphore. */
+    semThread = pthread_self();       /* let everyone know who has the semaphore. */
+  }
+#else
+  if ( GetCurrentThreadId() != semThread )
+  {
+    while (WaitForSingleObject(semHandle, 1000) != WAIT_OBJECT_0) ; /* wait for the semaphore. */
+    semThread = GetCurrentThreadId(); /* let everyone know who has the semaphore. */
+  }
+#endif
+  ++semDepth;                         /* increment semDepth - push one stack level */
+}
+
+
+void EF_rel_sem(void)
+{
+  if (!semInited)     EF_Abort("\nSemaphore isn't initialised");
+  if (!semThread)     EF_Abort("\nSemaphore isn't owned by this thread");
+  if (semDepth <= 0)  EF_Abort("\nSemaphore isn't locked");
+
+  if (!(--semDepth))              /* decrement semDepth - popping one stack level */
+  {
+#ifndef WIN32
+    semThread = (pthread_t) 0;        /* zero this before actually free'ing the semaphore. */
+    if (sem_post(&EF_sem) < 0)
+#else
+    semThread = 0;                    /* zero this before actually free'ing the semaphore. */
+    if (0 == ReleaseSemaphore(semHandle, 1 /* amount to add to current count */, NULL) )
+#endif
+      EF_Abort("Failed to post the semaphore.");
+  }
+}
+
+#else
+
+/* for not having an empty file */
+static int dummy = 0;
+
+#endif /* EF_NO_THREAD_SAFETY */
+
