127261637e3d31dddcdd03b5313a80691baff290
Hayati Ayguen <h_ayguen@web.de>
2005-09-18 01:34:35 +0000

    new operators now (nearly) full C++ conform

--- dumapp.cpp
+++ dumapp.cpp
@@ -27,8 +27,12 @@
 
 #ifndef DUMA_NO_CPP_SUPPORT
 
-
+#ifdef _MSC_VER
+#include <new.h>
 #include <new>
+#else
+#include <new>
+#endif
 
 #include <stdlib.h>
 
@@ -36,7 +40,7 @@
 
 
 #if WIN32
-#define DUMA_CDECL  __cdecl
+#define DUMA_CDECL     __cdecl
 #else
 #define DUMA_CDECL
 #endif
@@ -58,9 +62,114 @@ static const char unknown_cxx_file[] =
 #define DUMA_PARAMS_UK
 #endif
 
+/********************************************************************
+********************************************************************/
+
+#ifdef _MSC_VER
+
+int DUMA_CDECL duma_new_handler( size_t size )  { return 0; }
+
+#else
+
+void duma_new_handler() { }
+
+#endif
+
+
+/* C++ allocation function,
+ * which is nearly C++ conform. missing features are:
+ * 1- check for userSize == 0, return non-NULL in this case
+ * 2- check if new_handler function hangs, return NULL or throw bad_alloc() in this case
+ *    on UNIX systems alarm() may be usable
+ *
+ * following features are implemented:
+ * 1- loop is done
+ * 2- alignment depends on user
+ * 3- bad_alloc() exception will be thrown
+ * 4- bad_alloc() is catched to return (void*)0
+ *
+ */
+static inline
+void * duma_new_operator(size_t userSize, enum _DUMA_Allocator allocator, bool dothrow
+#ifndef DUMA_NO_LEAKDETECTION
+                         , const char * filename, int lineno
+#endif
+                        )
+#ifdef __GNUC__
+ __attribute__ ((always_inline))
+#endif
+{
+  void * ret = 0;
+#ifdef _MSC_VER
+  int     pnhret = 1;
+#else
+  #define pnhret 1
+#endif
+
+  // initialize duma?
+  if ( _duma_allocList == 0 )
+    _duma_init();  /* This sets DUMA_ALIGNMENT, DUMA_PROTECT_BELOW, DUMA_FILL, ... */
+
+  do
+  {
+    // try allocation
+    ret = _duma_allocate( 0 /*=alignment*/
+                        , userSize
+                        , DUMA_PROTECT_BELOW
+                        , DUMA_FILL
+                        , 1 /*=protectAllocList*/
+                        , allocator
+                        , DUMA_FAIL_NULL
+    #ifndef DUMA_NO_LEAKDETECTION
+                        , filename
+                        , lineno
+    #endif
+                        );
+
+    // when allocation failed (and last call to new_handler didn't fail on Visual C++)
+    if ( !ret && pnhret )
+    {
+      // get the current new_handler
+    #ifdef _MSC_VER
+      _PNH h = _set_new_handler(duma_new_handler);
+      _set_new_handler(h);
+    #else
+      std::new_handler h = std::set_new_handler(duma_new_handler);
+      std::set_new_handler(h);
+    #endif
+
+      // is there any handler?
+      if ( h )
+      {
+        try               // new_handler may throw an exception!!!
+        {
+          // call handler
+        #ifdef _MSC_VER
+          pnhret = h(userSize);
+        #else
+          h();
+        #endif
+        }
+        catch (std::bad_alloc)        // error occured in new_handler
+        {
+          if (dothrow)  throw;        // report error via re-throwing
+          else          return ret;   // report error via (void*)0
+        } // end try catch
+      }
+      else if (dothrow)   // report non-existing handler via exception
+        throw std::bad_alloc();
+      else                // report non-existing handler via (void*)0
+        return ret;
+    }
+  } while ( !ret );       // loop until memory is claimed
+  return ret;
+}
+
+
 
 /********************************************************************
 ********************************************************************/
+
 
 /* 1x : SINGLE OBJECT FORM - NO DEBUG INFORMATION */
 
@@ -68,10 +177,7 @@ static const char unknown_cxx_file[] =
 void * DUMA_CDECL operator new( DUMA_SIZE_T size )
 throw(std::bad_alloc)
 {
-  if ( _duma_allocList == 0 )  _duma_init();  /* This sets DUMA_ALIGNMENT, DUMA_PROTECT_BELOW, DUMA_FILL, ... */
-  void *ptr = _duma_allocate(0, size, DUMA_PROTECT_BELOW, DUMA_FILL, 1 /*=protectAllocList*/, EFA_NEW_ELEM, DUMA_FAIL_NULL  DUMA_PARAMS_UK);
-  if (!ptr) throw std::bad_alloc();
-  return ptr;
+  return duma_new_operator(size, EFA_NEW_ELEM, true  DUMA_PARAMS_UK);
 }
 
 
@@ -79,9 +185,7 @@ throw(std::bad_alloc)
 void * DUMA_CDECL operator new( DUMA_SIZE_T size, const std::nothrow_t & )
 throw()
 {
-  if ( _duma_allocList == 0 )  _duma_init();  /* This sets DUMA_ALIGNMENT, DUMA_PROTECT_BELOW, DUMA_FILL, ... */
-  void *ptr = _duma_allocate(0, size, DUMA_PROTECT_BELOW, DUMA_FILL, 1 /*=protectAllocList*/, EFA_NEW_ELEM, DUMA_FAIL_NULL  DUMA_PARAMS_UK);
-  return ptr;
+  return duma_new_operator(size, EFA_NEW_ELEM, false  DUMA_PARAMS_UK);
 }
 
 
@@ -112,10 +216,7 @@ throw()
 void * DUMA_CDECL operator new[]( DUMA_SIZE_T size )
 throw(std::bad_alloc)
 {
-  if ( _duma_allocList == 0 )  _duma_init();  /* This sets DUMA_ALIGNMENT, DUMA_PROTECT_BELOW, DUMA_FILL, ... */
-  void *ptr = _duma_allocate(0, size, DUMA_PROTECT_BELOW, DUMA_FILL, 1 /*=protectAllocList*/, EFA_NEW_ARRAY, DUMA_FAIL_NULL  DUMA_PARAMS_UK);
-  if (!ptr) throw std::bad_alloc();
-  return ptr;
+  return duma_new_operator(size, EFA_NEW_ARRAY, true  DUMA_PARAMS_UK);
 }
 
 
@@ -123,9 +224,7 @@ throw(std::bad_alloc)
 void * DUMA_CDECL operator new[]( DUMA_SIZE_T size, const std::nothrow_t & )
 throw()
 {
-  if ( _duma_allocList == 0 )  _duma_init();  /* This sets DUMA_ALIGNMENT, DUMA_PROTECT_BELOW, DUMA_FILL, ... */
-  void *ptr = _duma_allocate(0, size, DUMA_PROTECT_BELOW, DUMA_FILL, 1 /*=protectAllocList*/, EFA_NEW_ARRAY, DUMA_FAIL_NULL  DUMA_PARAMS_UK);
-  return ptr;
+  return duma_new_operator(size, EFA_NEW_ARRAY, false  DUMA_PARAMS_UK);
 }
 
 
@@ -158,19 +257,14 @@ throw()
 void * DUMA_CDECL operator new( DUMA_SIZE_T size, const char *filename, int lineno )
 throw( std::bad_alloc )
 {
-  if ( _duma_allocList == 0 )  _duma_init();  /* This sets DUMA_ALIGNMENT, DUMA_PROTECT_BELOW, DUMA_FILL, ... */
-  void *ptr = _duma_allocate(0, size, DUMA_PROTECT_BELOW, DUMA_FILL, 1 /*=protectAllocList*/, EFA_NEW_ELEM, DUMA_FAIL_NULL, filename, lineno);
-  if (!ptr) throw std::bad_alloc();
-  return ptr;
+  return duma_new_operator(size, EFA_NEW_ELEM, true, filename, lineno);
 }
 
 /* (32) = (b) ; ASN */
 void * DUMA_CDECL operator new( DUMA_SIZE_T size, const std::nothrow_t &, const char *filename, int lineno )
 throw()
 {
-  if ( _duma_allocList == 0 )  _duma_init();  /* This sets DUMA_ALIGNMENT, DUMA_PROTECT_BELOW, DUMA_FILL, ... */
-  void *ptr = _duma_allocate(0, size, DUMA_PROTECT_BELOW, DUMA_FILL, 1 /*=protectAllocList*/, EFA_NEW_ELEM, DUMA_FAIL_NULL, filename, lineno);
-  return ptr;
+  return duma_new_operator(size, EFA_NEW_ELEM, false, filename, lineno);
 }
 
 
@@ -200,10 +294,7 @@ throw()
 void * DUMA_CDECL operator new[]( DUMA_SIZE_T size, const char *filename, int lineno )
 throw( std::bad_alloc )
 {
-  if ( _duma_allocList == 0 )  _duma_init();  /* This sets DUMA_ALIGNMENT, DUMA_PROTECT_BELOW, DUMA_FILL, ... */
-  void *ptr = _duma_allocate(0, size, DUMA_PROTECT_BELOW, DUMA_FILL, 1 /*=protectAllocList*/, EFA_NEW_ARRAY, DUMA_FAIL_NULL, filename, lineno);
-  if (!ptr) throw std::bad_alloc();
-  return ptr;
+  return duma_new_operator(size, EFA_NEW_ARRAY, true, filename, lineno);
 }
 
 
@@ -211,9 +302,7 @@ throw( std::bad_alloc )
 void * DUMA_CDECL operator new[]( DUMA_SIZE_T size, const std::nothrow_t &, const char *filename, int lineno )
 throw()
 {
-  if ( _duma_allocList == 0 )  _duma_init();  /* This sets DUMA_ALIGNMENT, DUMA_PROTECT_BELOW, DUMA_FILL, ... */
-  void *ptr = _duma_allocate(0, size, DUMA_PROTECT_BELOW, DUMA_FILL, 1 /*=protectAllocList*/, EFA_NEW_ARRAY, DUMA_FAIL_NULL, filename, lineno);
-  return ptr;
+  return duma_new_operator(size, EFA_NEW_ARRAY, false, filename, lineno);
 }
 
 
