116532bbb3bbdfd84beeb0262e084ed9c9d6d38d
Hayati Ayguen <h_ayguen@web.de>
2005-09-27 06:24:12 +0000

    modified to allow inclusion, even within function scope

--- duma.h
+++ duma.h
@@ -39,14 +39,20 @@
 #include "duma_config.h"
 #endif
 
+#ifdef __cplusplus
+#define DUMA_EXTERN_C   extern "C"
+#else
+#define DUMA_EXTERN_C   extern
+#endif
+
 
 #ifdef DUMA_NO_DUMA
 
 /* enable these macros even in release code, but do nothing */
-#define DUMA_newFrame()               do { } while(0)
-#define DUMA_delFrame()               do { } while(0)
+#define DUMA_newFrame()             do { } while(0)
+#define DUMA_delFrame()             do { } while(0)
 
-#define DUMA_ASSERT(EXPR)             do { } while(0)
+#define DUMA_ASSERT(EXPR)           do { } while(0)
 
 #define CA_DECLARE(NAME,SIZE)       do { } while(0)
 #define CA_DEFINE(TYPE,NAME,SIZE)   TYPE NAME[SIZE]
@@ -54,21 +60,20 @@
 
 #else /* ifndef DUMA_NO_DUMA */
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
+#ifndef DUMA_EXTERNS_DECLARED
+#define DUMA_EXTERNS_DECLARED
 /* global DUMA variables */
-extern int  DUMA_PROTECT_BELOW;
-extern int  DUMA_ALIGNMENT;
-extern int  DUMA_FILL;
-extern struct _DUMA_Slot * _duma_allocList;
+DUMA_EXTERN_C int  DUMA_PROTECT_BELOW;
+DUMA_EXTERN_C int  DUMA_ALIGNMENT;
+DUMA_EXTERN_C int  DUMA_FILL;
+DUMA_EXTERN_C struct _DUMA_Slot * _duma_allocList;
 #ifndef DUMA_NO_CPP_SUPPORT
-extern void * _duma_cxx_null_addr;
+DUMA_EXTERN_C void * _duma_cxx_null_addr;
 #endif
+#endif /* DUMA_EXTERNS_DECLARED */
 
-#ifndef _DUMA_ENUMS
-#define _DUMA_ENUMS
+#ifndef DUMA_ENUMS_DECLARED
+#define DUMA_ENUMS_DECLARED
 
 enum _DUMA_Allocator
 {
@@ -101,79 +106,75 @@ enum _DUMA_FailReturn
   , DUMA_FAIL_ENV
 };
 
-#endif /* _DUMA_ENUMS */
+#endif /* DUMA_ENUMS_DECLARED */
 
 
-#ifdef DUMA_EXPLICIT_INIT
-void   duma_init(void);
-#endif
+#ifndef DUMA_FUNCTIONS_DECLARED
+#define DUMA_FUNCTIONS_DECLARED
 
-void   _duma_init(void);
+  DUMA_EXTERN_C void _duma_init(void);
+  DUMA_EXTERN_C void _duma_assert(const char * exprstr, const char * filename, int lineno);
+
+  #ifdef DUMA_EXPLICIT_INIT
+    DUMA_EXTERN_C void   duma_init(void);
+  #endif
+
+  #ifndef DUMA_NO_LEAKDETECTION
+    DUMA_EXTERN_C void * _duma_allocate(size_t alignment, size_t userSize, int protectBelow, int fillByte, int protectAllocList, enum _DUMA_Allocator allocator, enum _DUMA_FailReturn fail, const char * filename, int lineno);
+    DUMA_EXTERN_C void   _duma_deallocate(void * baseAdr, int protectAllocList, enum _DUMA_Allocator allocator, const char * filename, int lineno);
+    DUMA_EXTERN_C void * _duma_malloc(size_t size, const char * filename, int lineno);
+    DUMA_EXTERN_C void * _duma_calloc(size_t elemCount, size_t elemSize, const char * filename, int lineno);
+    DUMA_EXTERN_C void   _duma_free(void * baseAdr, const char * filename, int lineno);
+    DUMA_EXTERN_C void * _duma_memalign(size_t alignment, size_t userSize, const char * filename, int lineno);
+    DUMA_EXTERN_C void * _duma_realloc(void * baseAdr, size_t newSize, const char * filename, int lineno);
+    DUMA_EXTERN_C void * _duma_valloc(size_t size, const char * filename, int lineno);
+    DUMA_EXTERN_C char * _duma_strdup(const char *str, const char * filename, int lineno);
+    DUMA_EXTERN_C void * _duma_memcpy(void *dest, const void *src, size_t size, const char * filename, int lineno);
+    DUMA_EXTERN_C char * _duma_strcpy(char *dest, const char *src, const char * filename, int lineno);
+    DUMA_EXTERN_C char * _duma_strncpy(char *dest, const char *src, size_t size, const char * filename, int lineno);
+    DUMA_EXTERN_C char * _duma_strcat(char *dest, const char *src, const char * filename, int lineno);
+    DUMA_EXTERN_C char * _duma_strncat(char *dest, const char *src, size_t size, const char * filename, int lineno);
+    DUMA_EXTERN_C void  DUMA_newFrame(void);
+    DUMA_EXTERN_C void  DUMA_delFrame(void);
+  #else /* DUMA_NO_LEAKDETECTION */
+    DUMA_EXTERN_C void * _duma_allocate(size_t alignment, size_t userSize, int protectBelow, int fillByte, int protectAllocList, enum _DUMA_Allocator allocator, enum _DUMA_FailReturn fail);
+    DUMA_EXTERN_C void   _duma_deallocate(void * baseAdr, int protectAllocList, enum _DUMA_Allocator allocator);
+    DUMA_EXTERN_C void * _duma_malloc(size_t size);
+    DUMA_EXTERN_C void * _duma_calloc(size_t elemCount, size_t elemSize);
+    DUMA_EXTERN_C void   _duma_free(void * baseAdr);
+    DUMA_EXTERN_C void * _duma_memalign(size_t alignment, size_t userSize);
+    DUMA_EXTERN_C void * _duma_realloc(void * baseAdr, size_t newSize);
+    DUMA_EXTERN_C void * _duma_valloc(size_t size);
+    DUMA_EXTERN_C char * _duma_strdup(const char *str);
+    DUMA_EXTERN_C void * _duma_memcpy(void *dest, const void *src, size_t size);
+    DUMA_EXTERN_C char * _duma_strcpy(char *dest, const char *src);
+    DUMA_EXTERN_C char * _duma_strncpy(char *dest, const char *src, size_t size);
+    DUMA_EXTERN_C char * _duma_strcat(char *dest, const char *src);
+    DUMA_EXTERN_C char * _duma_strncat(char *dest, const char *src, size_t size);
+  #endif /* DUMA_NO_LEAKDETECTION */
+
+#endif /* DUMA_FUNCTIONS_DECLARED */
 
 
 #ifndef DUMA_NO_LEAKDETECTION
-
-void * _duma_allocate(size_t alignment, size_t userSize, int protectBelow, int fillByte, int protectAllocList, enum _DUMA_Allocator allocator, enum _DUMA_FailReturn fail, const char * filename, int lineno);
-void   _duma_deallocate(void * baseAdr, int protectAllocList, enum _DUMA_Allocator allocator, const char * filename, int lineno);
-
-void * _duma_malloc(size_t size, const char * filename, int lineno);
-void * _duma_calloc(size_t elemCount, size_t elemSize, const char * filename, int lineno);
-void   _duma_free(void * baseAdr, const char * filename, int lineno);
-void * _duma_memalign(size_t alignment, size_t userSize, const char * filename, int lineno);
-void * _duma_realloc(void * baseAdr, size_t newSize, const char * filename, int lineno);
-void * _duma_valloc(size_t size, const char * filename, int lineno);
-char * _duma_strdup(const char *str, const char * filename, int lineno);
-void * _duma_memcpy(void *dest, const void *src, size_t size, const char * filename, int lineno);
-char * _duma_strcpy(char *dest, const char *src, const char * filename, int lineno);
-char * _duma_strncpy(char *dest, const char *src, size_t size, const char * filename, int lineno);
-char * _duma_strcat(char *dest, const char *src, const char * filename, int lineno);
-char * _duma_strncat(char *dest, const char *src, size_t size, const char * filename, int lineno);
-
-
-void  DUMA_newFrame(void);
-void  DUMA_delFrame(void);
-
-#define malloc(SIZE)                _duma_malloc(SIZE, __FILE__, __LINE__)
-#define calloc(ELEMCOUNT, ELEMSIZE) _duma_calloc(ELEMCOUNT, ELEMSIZE, __FILE__, __LINE__)
-#define free(BASEADR)               _duma_free(BASEADR, __FILE__, __LINE__)
-#define memalign(ALIGNMENT, SIZE)   _duma_memalign(ALIGNMENT, SIZE, __FILE__, __LINE__)
-#define realloc(BASEADR, NEWSIZE)   _duma_realloc(BASEADR, NEWSIZE, __FILE__, __LINE__)
-#define valloc(SIZE)                _duma_valloc(SIZE, __FILE__, __LINE__)
-#define strdup(STR)                 _duma_strdup(STR, __FILE__, __LINE__)
-#define memcpy(DEST, SRC, SIZE)     _duma_memcpy(DEST, SRC, SIZE, __FILE__, __LINE__)
-#define strcpy(DEST, SRC)           _duma_strcpy(DEST, SRC, __FILE__, __LINE__)
-#define strncpy(DEST, SRC, SIZE)    _duma_strncpy(DEST, SRC, SIZE, __FILE__, __LINE__)
-#define strcat(DEST, SRC)           _duma_strcat(DEST, SRC, __FILE__, __LINE__)
-#define strncat(DEST, SRC, SIZE)    _duma_strncat(DEST, SRC, SIZE, __FILE__, __LINE__)
-
+  #define malloc(SIZE)                _duma_malloc(SIZE, __FILE__, __LINE__)
+  #define calloc(ELEMCOUNT, ELEMSIZE) _duma_calloc(ELEMCOUNT, ELEMSIZE, __FILE__, __LINE__)
+  #define free(BASEADR)               _duma_free(BASEADR, __FILE__, __LINE__)
+  #define memalign(ALIGNMENT, SIZE)   _duma_memalign(ALIGNMENT, SIZE, __FILE__, __LINE__)
+  #define realloc(BASEADR, NEWSIZE)   _duma_realloc(BASEADR, NEWSIZE, __FILE__, __LINE__)
+  #define valloc(SIZE)                _duma_valloc(SIZE, __FILE__, __LINE__)
+  #define strdup(STR)                 _duma_strdup(STR, __FILE__, __LINE__)
+  #define memcpy(DEST, SRC, SIZE)     _duma_memcpy(DEST, SRC, SIZE, __FILE__, __LINE__)
+  #define strcpy(DEST, SRC)           _duma_strcpy(DEST, SRC, __FILE__, __LINE__)
+  #define strncpy(DEST, SRC, SIZE)    _duma_strncpy(DEST, SRC, SIZE, __FILE__, __LINE__)
+  #define strcat(DEST, SRC)           _duma_strcat(DEST, SRC, __FILE__, __LINE__)
+  #define strncat(DEST, SRC, SIZE)    _duma_strncat(DEST, SRC, SIZE, __FILE__, __LINE__)
 #else /* DUMA_NO_LEAKDETECTION */
-
-void * _duma_allocate(size_t alignment, size_t userSize, int protectBelow, int fillByte, int protectAllocList, enum _DUMA_Allocator allocator, enum _DUMA_FailReturn fail);
-void   _duma_deallocate(void * baseAdr, int protectAllocList, enum _DUMA_Allocator allocator);
-
-void * _duma_malloc(size_t size);
-void * _duma_calloc(size_t elemCount, size_t elemSize);
-void   _duma_free(void * baseAdr);
-void * _duma_memalign(size_t alignment, size_t userSize);
-void * _duma_realloc(void * baseAdr, size_t newSize);
-void * _duma_valloc(size_t size);
-char * _duma_strdup(const char *str);
-void * _duma_memcpy(void *dest, const void *src, size_t size);
-char * _duma_strcpy(char *dest, const char *src);
-char * _duma_strncpy(char *dest, const char *src, size_t size);
-char * _duma_strcat(char *dest, const char *src);
-char * _duma_strncat(char *dest, const char *src, size_t size);
-
-#define DUMA_newFrame() do { } while(0)
-#define DUMA_delFrame() do { } while(0)
-
+  #define DUMA_newFrame()             do { } while(0)
+  #define DUMA_delFrame()             do { } while(0)
 #endif /* DUMA_NO_LEAKDETECTION */
 
-
 #ifndef DUMA_ASSERT
-  /* declare some special assert only once */
-  void _duma_assert(const char * exprstr, const char * filename, int lineno);
-
   #define DUMA_ASSERT(EXPR)    (  (EXPR) || ( _duma_assert(#EXPR, __FILE__, __LINE__), 0 )  )
 #endif
 
@@ -221,9 +222,5 @@ unsigned long NAME ## _checkedidx
 #define CA_REF(NAME,INDEX) \
 NAME[ DUMA_ASSERT( (NAME ## _checkedidx = (INDEX)) < NAME ## _checkedsize ), NAME ## _checkedidx ]
 
-
-#ifdef __cplusplus
-} /* extern "C" */
-#endif
 
 #endif /* end ifdef DUMA_NO_DUMA */
