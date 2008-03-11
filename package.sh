1ff8207e081fd3f2235d686bac02f3454d7a0962
Hayati Ayguen <h_ayguen@web.de>
2008-03-11 22:31:54 +0000

    automation of upload

--- package.sh
+++ package.sh
@@ -1,12 +1,84 @@
 #!/bin/sh
-echo usage: ./package.sh 2_4_12
 export CVSROOT=":pserver:anonymous@duma.cvs.sourceforge.net:/cvsroot/duma"
-rm -Rf duma duma_$1 duma_$1.zip duma_$1.tar.gz
-cvs co -r VERSION_$1 duma
-date >duma/VERSION
-echo DUMA $1 >>duma/VERSION
+
+#translate system user to sourceforge username
+SYS_USER=`whoami`
+SF_USER=
+if (test "$SYS_USER" = "ayguen" ); then
+  SF_USER=h_ayguen
+fi
+
+if (test "$1" = ""); then
+  echo "usage: $0 2_5_13|HEAD [upload] [htdocs]"
+  exit 0;
+elif (test "$1" = "HEAD"); then
+  VER=`date -u +%Y%m%d_%H%M%S`
+  REV=HEAD
+else
+  VER=$1
+  REV=VERSION_$1
+fi
+
+rm -Rf duma duma_$VER duma_$VER.zip duma_$VER.tar.gz
+cvs co -r $REV duma
+date -u -R >duma/VERSION
+echo DUMA $VER $REV >>duma/VERSION
 echo "Actual version available from http://duma.sourceforge.net/" >>duma/VERSION
-mv duma duma_$1
-zip -9 -r duma_$1.zip duma_$1
-tar zcf duma_$1.tar.gz duma_$1
-rm -Rf duma_$1
+mv duma duma_$VER
+zip -9 -r duma_$VER.zip duma_$VER
+tar zcf duma_$VER.tar.gz duma_$VER
+
+if (test "$SF_USER" = "" ); then
+  echo invalid SF_USER $SF_USER
+fi
+
+cat >ftp_upload.pl <<EOF
+#!/usr/bin/perl
+use Net::FTP;
+
+if ( @ARGV <= 0 ) {
+  print "no file argument given!\n";
+  exit 10;
+}
+
+my \$file = "@ARGV[0]";
+my \$host = 'upload.sourceforge.net';
+my \$user = 'anonymous';
+my \$password = 'anonymous';
+my \$dir = '/incoming';
+
+# Neues Net::FTP-Objekt
+my \$ftp = Net::FTP->new(\$host,
+                        Timeout => 360,
+                        Debug   => 0
+                       );
+unless (defined \$ftp) {
+	print "\$@\n";
+	die "Can't create Net::FTP-Object\n";
+}
+
+\$ftp->login(\$user,\$password) || die "Can't login \$!";
+\$ftp->cwd(\$dir) || die "Can't change working directory \$dir \$!";
+\$ftp->type(I);
+\$remote_file_name = \$ftp->put(\$file) || die "Can't put \$file \$!";
+\$ftp->quit();
+EOF
+
+if ((test "$2" = "upload") || (test "$3" = "upload")); then
+  chmod a+x ftp_upload.pl
+  echo uploading file duma_$VER.zip
+  ./ftp_upload.pl duma_$VER.zip
+  echo uploading file duma_$VER.tar.gz
+  ./ftp_upload.pl duma_$VER.tar.gz
+fi
+
+if ((test "$2" = "htdocs") || (test "$3" = "htdocs")); then
+  echo updating sourceforge htdocs
+  scp duma_$VER/README.txt $SF_USER@shell.sourceforge.net:/home/groups/d/du/duma/htdocs/README.txt
+  scp duma_$VER/INSTALL    $SF_USER@shell.sourceforge.net:/home/groups/d/du/duma/htdocs/INSTALL
+  scp duma_$VER/CHANGELOG  $SF_USER@shell.sourceforge.net:/home/groups/d/du/duma/htdocs/CHANGELOG
+  scp duma_$VER/TODO       $SF_USER@shell.sourceforge.net:/home/groups/d/du/duma/htdocs/TODO
+fi
+
+rm -f ./ftp_upload.pl
+rm -Rf duma_$VER
