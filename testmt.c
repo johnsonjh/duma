3a89d8d3c7bae9ad822d1d37648ecceeb904a2d4
Hayati Ayguen <h_ayguen@web.de>
2008-08-03 19:10:54 +0000

    added Windows Threading to testmt.c

--- testmt.c
+++ testmt.c
@@ -3,75 +3,137 @@
  * test program from
  * Debian Bug report logs - #241156
  * electric-fence (2.1.13-0.1) breaks with 2.6.x kernel
+ *
+ * changed to compile on Windows by Hayati Ayguen
  */
 
 /* $Id$ */
 /* gcc foo.c -pthread -lefence -g -ggdb -o foo */
 
-#if (defined(_MSC_VER) || defined(__MINGW32__) || defined(__MINGW64__))
-/* (defined(WIN32) || defined(__CYGWIN__) || defined(__MINGW32__) || defined(__MINGW64__)) */
-/* no pthread library on windows!
- * but on cygwin
- * so skip this test for now
- */
 
-int main(int argc, char *argv[])
-{
-  printf("Test not implemented for Win32, Cygwin and Mingw\n");
-  return 0;
-}
+/* (defined(WIN32) || defined(__CYGWIN__) || defined(__MINGW32__) || defined(__MINGW64__)) */
+#if ( defined(_MSC_VER) || defined(__MINGW32__) || defined(__MINGW64__) )
+
+#include <windows.h>
 
 #else
 
 #include <pthread.h>
+#include <unistd.h>
+
+#define HAVE_PTHREADS
+
+#endif
+
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include "duma.h"
+
 
 volatile int iKillThreads = 0;
 
+
+/* thread function
+ * permanently allocate, initialize and deallocate in a loop
+ */
+#ifdef  HAVE_PTHREADS
 void* poster(void* arg)
+#else
+DWORD WINAPI poster( LPVOID arg )
+#endif
 {
   char* foo = NULL;
   (void)arg;
   for( ; !iKillThreads; )
   {
-    foo = (char*) malloc(4096);
+    foo = (char*)malloc(4096);
     if (foo)
+    {
+      memset(foo, 0, 4096);
       free(foo);
+    }
   }
-  return(NULL);
+  return 0;
 }
+
+
+#define MAX_NUM_THREADS   32
 
 int main(int argc, char *argv[])
 {
-  int iSleepTime = 60;  /* in seconds; default = 10 sec */
-  pthread_t ida;
-  pthread_t idb;
+  int iSleepTime  = 60; /* in seconds; default = 30 sec */
+  int iNumThreads = 4;
+  int i;
 
-  if ( argc >= 2 )
+#ifdef  HAVE_PTHREADS
+  pthread_t tId[MAX_NUM_THREADS];
+#else
+  HANDLE    tId[MAX_NUM_THREADS];
+#endif
+
+  if ( argc > 1 )
   {
-    iSleepTime = atoi(argv[1]);
-    if ( iSleepTime <= 0 )
-      iSleepTime = 10;
+    i = atoi(argv[1]);
+    if ( i > 0 )
+      iSleepTime = i;
+  }
+  if ( argc > 2 )
+  {
+    i = atoi(argv[2]);
+    if ( i > 0 )
+      iNumThreads = i;
+    if ( iNumThreads > MAX_NUM_THREADS )
+      iNumThreads = MAX_NUM_THREADS;
   }
 
-  fprintf(stdout, "running 2 threads for %d secs ..", iSleepTime);
+  fprintf(stdout, "running %d threads for %d secs ..", iNumThreads, iSleepTime);
   fflush(stdout);
 
-  pthread_create(&ida, NULL, poster, NULL);
-  pthread_create(&idb, NULL, poster, NULL);
+#ifdef  HAVE_PTHREADS
+  for ( i = 0; i < iNumThreads; ++i )
+    pthread_create(&tId[i], NULL, poster, NULL);
 
-  sleep(iSleepTime);
+  /* sleep(iSleepTime); */
+  for ( i = 0; i < iSleepTime; ++i )
+  {
+    sleep(1);  /* wait 1 sec  */
+    fprintf(stdout, ".");
+    fflush(stdout);
+  }
+#else
+  for ( i = 0; i < iNumThreads; ++i )
+    tId[i] = CreateThread( NULL    /* default security attributes */
+                    , 0       /* use default stack size */
+                    , poster  /* thread function name */
+                    , NULL    /* argument to thread function */
+                    , 0       /* use default creation flags */
+                    , NULL    /* returns the thread identifier */
+                    );
+  /* Sleep(iSleepTime*1000); */
+  for ( i =0; i < iSleepTime; ++i )
+  {
+    Sleep(1000);  /* wait 1000 ms  */
+    fprintf(stdout, ".");
+    fflush(stdout);
+  }
+#endif
+
   fprintf(stdout, "..done\n");
   iKillThreads = 1;
   fprintf(stdout, "wating threads to end ..");
   fflush(stdout);
-  pthread_join(ida, NULL);
-  pthread_join(idb, NULL);
+
+#ifdef  HAVE_PTHREADS
+  for ( i = 0; i < iNumThreads; ++i )
+    pthread_join(tId[i], NULL);
+#else
+  // Wait until all threads have terminated.
+  WaitForMultipleObjects( iNumThreads, tId, TRUE, INFINITE);
+  for ( i = 0; i < iNumThreads; ++i )
+    CloseHandle(tId[i]);
+#endif
   fprintf(stdout, "..done\n");
   return 0;
 }
 
-#endif
