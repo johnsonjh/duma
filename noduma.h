32e44b70e3a69ec32c2242fad30c02bdf2514d64
Hayati Ayguen <h_ayguen@web.de>
2009-06-15 16:47:20 +0000

    removed unnecessary eof comment

--- noduma.h
+++ noduma.h
@@ -1,6 +1,6 @@
 /*
  * DUMA - Red-Zone memory allocator.
- * Copyright (C) 2002-2008 Hayati Ayguen <h_ayguen@web.de>, Procitec GmbH
+ * Copyright (C) 2002-2009 Hayati Ayguen <h_ayguen@web.de>, Procitec GmbH
  * Copyright (C) 1987-1999 Bruce Perens <bruce@perens.com>
  * License: GNU GPL (GNU General Public License, see COPYING-GPL)
  *
@@ -205,6 +205,4 @@
  *
  */
 
-/***************/
-/* END OF FILE */
-/***************/
+
