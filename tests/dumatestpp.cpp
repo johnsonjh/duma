44597418e5f681ab11bc5693591c2272034399cb
Hayati Ayguen <h_ayguen@web.de>
2009-07-05 23:09:23 +0000

    move to tests/

---
+++ tests/dumatestpp.cpp
@@ -0,0 +1,153 @@
+
+#include <stdio.h>
+
+#include <new>
+#include <fstream>
+
+#ifndef DUMA_SO_LIBRARY
+#include "dumapp.h"
+#endif
+
+
+#if 0
+
+class test
+{
+public:
+  test()
+  {
+    x = 0;
+  }
+
+  test(int m)
+  {
+    x = m;
+  }
+
+  ~test()
+  {
+    x = 0;
+  }
+
+  int x;
+};
+
+class testtest
+{
+public:
+  testtest()
+  {
+    y = new test;     //NEW_ELEM( test );   //new test();
+  }
+
+  testtest(int m)
+  {
+    y = new test(m);  //NEW_ELEM( test(m) );  //new test(m);
+  }
+
+  ~testtest()
+  {
+    delete y;
+  }
+
+  test *y;
+};
+#endif
+
+int main( int argc, char ** argv )
+{
+  (void)argc;
+  (void)argv;
+
+#if 0
+  int * x = 0;
+  int ret;
+  ret = posix_memalign(&x, 2048, sizeof(int) );
+  if ( !ret )
+    *x = 1;
+  free(x);
+#elif 0
+#include "noduma.h"
+  int * x = 0;
+  x = new int[10];
+  x[0] = 0;
+  delete x;
+#elif 0
+	std::fstream *f = new std::fstream();
+	delete f;
+
+#elif 0
+  /* this test should fail */
+  int i;
+  CA_DEFINE(int,acTest,20);
+
+  for (i=0; i<100; ++i)
+    CA_REF(acTest,i) = i;
+#elif 0
+  /* this test should not fail */
+  int *y, *z;
+
+  y = new int;
+  z = new int[2];
+  DEL_ARRAY( z );
+  DEL_ARRAY( z );
+  DEL_ELEM( y );
+  DEL_ELEM( y );
+
+#elif 1
+
+  int *y, *z;
+
+  y = new int;
+  z = new int[2];
+  delete []z;
+  delete y;
+
+#elif 0
+  int *y, *z;
+
+  y = new int;
+  z = new int[2];
+  DEL_ARRAY(z);
+  DEL_ELEM(y);
+
+  //y = new int;
+  //DEL_ELEM(y);
+  //DEL_ELEM(y);
+
+  //y = new(__FILE__,__LINE__) int;
+  //delete (__FILE__, __LINE__, y);
+
+  //y = new(std::nothrow) int;
+  //delete (std::nothrow, y);
+
+  //y = new(std::nothrow, __FILE__, __LINE__) int;
+  //delete (std::nothrow, __FILE__, __LINE__, y);
+#elif 0
+  /* this test should not fail */
+  int *y;
+  y = NEW_ARRAY( int, 2 );
+  DEL_ARRAY( y );
+#elif 0
+  /* this test should not fail */
+  int * x, *y;
+  test *z;
+  x = (int*)malloc( sizeof(int) );
+  y = new int;
+  z = new test[2];
+  free( x );
+  delete y;
+  delete []z;
+#elif 0
+  /* this test should not fail */
+  test * x = new test(10);
+  test *ax = new test[2];
+  testtest *y = new testtest;
+  delete y;
+  delete []ax;
+  delete x;
+#endif
+  return 0;
+
+}
+
---
+++ tests/thread-test.c
@@ -0,0 +1,139 @@
+/*
+ * source URL:
+ * http://bugs.debian.org/cgi-bin/bugreport.cgi?bug=67838
+ */
+
+/*
+ * Author:  Petter Reinholdtsen <pere@hungry.com>
+ * Date:    2002-02-13
+ * Lisence: GPL
+ *
+ * Small test program to demonstrate efence crashing on multithreaded
+ * programs.
+
+ * Link it like this, and it crashes with segfault.
+ *  cc -o efence-thread-test efence-thread-test.c  -lpthread -lefence
+ * Remove the -lefence, and it work as it should.
+ * The output from gdb when it fails:
+ *
+ * Program received signal SIGSEGV, Segmentation fault.
+ * [Switching to Thread 1026 (LWP 1189)]
+ * 0x400382c7 in memalign () from /usr/lib/libefence.so.0
+ * (gdb) bt
+ * #0  0x400382c7 in memalign () from /usr/lib/libefence.so.0
+ * #1  0x4003873c in malloc () from /usr/lib/libefence.so.0
+ * #2  0x400a0811 in fopen () from /lib/libc.so.6
+ * #3  0x080488e8 in thread_func ()
+ * #4  0x40027f0a in pthread_start_thread () from /lib/libpthread.so.0
+ * #5  0x40027f51 in pthread_start_thread_event () from /lib/libpthread.so.0
+ */
+
+/* check for pthread library */
+#if (!defined(WIN32) || defined(__CYGWIN__))
+#define HAVE_PTHREADS 1
+#else
+#define HAVE_PTHREADS 0
+#endif
+
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#if HAVE_PTHREADS
+
+#include <pthread.h>
+#include "duma.h"
+
+static pthread_mutex_t mutex;
+static volatile int threads_left = 2;
+
+
+static void* thread_func(void *arg)
+{
+  int i = 2000;
+  char *name = (char*)arg;
+
+  while (--i)
+  {
+    if (pthread_mutex_lock(&mutex))
+    {
+      fprintf(stderr, "error: %s failed to lock mutex.\n", name);
+      exit(1);
+    }
+    printf ("%s : %d\n", name, i);
+    if (pthread_mutex_unlock(&mutex))
+    {
+      fprintf(stderr, "error: %s failed to unlock mutex.\n", name);
+      exit(1);
+    }
+
+    {
+      /* Try to trigger efence error */
+      FILE *fp = fopen("/etc/resolv.conf", "r");
+      if (NULL != fp)
+      {
+        char buf[1024];
+        fread(buf, sizeof(buf), 1, fp);
+        fclose(fp);
+      }
+    }
+  }
+  --threads_left;
+  return NULL;
+}
+
+
+static void*
+idle_func(void* arg)
+{
+  (void)arg;
+
+  while (threads_left) ;
+
+  /* NOTREACHED */
+  return NULL;
+}
+
+
+int main(int argc, char **argv)
+{
+  pthread_t hello_thread, goodbye_thread;
+
+  (void)argc;
+  (void)argv;
+
+  pthread_mutex_init(&mutex, NULL);
+
+  if (pthread_create(&hello_thread, NULL, thread_func, (void*)"hello"))
+  {
+    fprintf(stderr, "Failed to create hello thread\n");
+    exit(1);
+  }
+
+  if (pthread_create(&goodbye_thread, NULL, thread_func, (void*)"goodbye"))
+  {
+    fprintf(stderr, "Failed to create goodbye thread\n");
+    exit(1);
+  }
+
+  idle_func(NULL);
+
+  pthread_mutex_destroy(&mutex);
+
+  /* NOTREACHED */
+  return 0;
+}
+
+#else
+
+int main(int argc, char **argv)
+{
+  (void)argc;
+  (void)argv;
+
+  printf("Test not implemented for Win32 and Mingw\n");
+  return 0;
+}
+
+#endif
+
