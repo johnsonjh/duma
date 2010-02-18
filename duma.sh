c0b086bf7b75d13a1f0c96a2c5d3e2812c93dcd7
Moritz Barsnick <moritz@barsnick.net>
2010-02-18 00:00:00 -0500

    Fix for duma.sh, for arguments containing spaces.
    Also some additional general clean-up, and the
    elimination of the use of an unnecessary sub-shell.

--- duma.sh
+++ duma.sh
@@ -1,18 +1,17 @@
-#!/bin/bash
+#!/usr/bin/env sh
 
-#
-# Simple script to run DUMA on stuff.  (needs libduma.so).
-#
-# $Id$
-#
+# Simple script to run DUMA on stuff.  
+# (Requires libduma.so.0.0 insalled).
 
-# Usage check.
-if [ "$1" = "" ]; then
-	echo "$0: Usage: duma [executable] [arguments]."
-	echo "	Runs the executable under the DUMA malloc debugger."
-	exit
+# Usage.
+if [ "$#" -lt 1 ]; then
+	printf '%s\n' "$0:	Usage: duma <executable> [optional arguments]."
+	printf '%s\n' "		Runs the executable under the DUMA malloc debugger."
+	exit 1
 fi
 
-# Set the environment to (pre)load duma library.
-(export LD_PRELOAD=libduma.so.0.0.0; export DYLD_INSERT_LIBRARIES=libduma.dylib; export DYLD_FORCE_FLAT_NAMESPACE=1; exec $*)
-
+# Set the environment to preload DUMA library.
+export LD_PRELOAD=libduma.so.0.0.0
+export DYLD_INSERT_LIBRARIES=libduma.dylib
+export DYLD_FORCE_FLAT_NAMESPACE=1
+exec "$@"
