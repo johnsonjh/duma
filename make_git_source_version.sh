4b167031db261256a4d535c4cd372df068c053ed
Restyled.io <commits@restyled.io>
2021-05-30 08:39:54 +0000

    Restyled by shellharden

--- make_git_source_version.sh
+++ make_git_source_version.sh
@@ -14,9 +14,9 @@ get_git_info()
 		if command git version 2> /dev/null 1>&2; then
 			GITTEST=$(git version 2> /dev/null)
 			# shellcheck disable=SC2236
-			if [ -n "${GITTEST}" ] && [ ! -z "${GITTEST}" ]; then
+			if [ -n "$GITTEST" ] && [ ! -z "$GITTEST" ]; then
 				BRANCH=$(git branch --show-current 2> /dev/null)
-				if [ -n "${RELEASED}" ] && [ ! -z "${RELEASED}" ]; then
+				if [ -n "$RELEASED" ] && [ ! -z "$RELEASED" ]; then
 					GITVER=$(git describe --tags --always 2> /dev/null |
 						cut -d "-" -f 1 2> /dev/null)
 				else
@@ -24,31 +24,31 @@ get_git_info()
 						2> /dev/null)
 				fi
 
-				if [ ! -n "${BRANCH}" ] || [ -z "${BRANCH}" ]; then
+				if [ ! -n "$BRANCH" ] || [ -z "$BRANCH" ]; then
 					BRANCH="nobranch"
 				fi
 
-				if [ ! -n "${RELEASED}" ] && [ -z "${RELEASED}" ]; then
-					if [ -n "${GITVER}" ] && [ ! -z "${GITVER}" ]; then
-						GIT_OUT=" ${GITVER}-${BRANCH}"
+				if [ ! -n "$RELEASED" ] && [ -z "$RELEASED" ]; then
+					if [ -n "$GITVER" ] && [ ! -z "$GITVER" ]; then
+						GIT_OUT=" $GITVER-$BRANCH"
 					fi
 				fi
-				if [ -n "${GITVER}" ] && [ ! -z "${GITVER}" ]; then
-					GIT_OUT=" ${GITVER}"
+				if [ -n "$GITVER" ] && [ ! -z "$GITVER" ]; then
+					GIT_OUT=" $GITVER"
 				fi
 			fi
 		fi
 	fi
 
-	GIT_SOURCE_INFO="DUMA${GIT_OUT}"
-	GIT_SOURCE_XFRM=$(printf '%s\n' "${GIT_SOURCE_INFO}" |
+	GIT_SOURCE_INFO="DUMA$GIT_OUT"
+	GIT_SOURCE_XFRM=$(printf '%s\n' "$GIT_SOURCE_INFO" |
 		sed -e 's/\VERSION_//' -e 's/_/\./g' 2> /dev/null)
 
 	# shellcheck disable=SC2236
-	if [ -n "${GIT_SOURCE_XFRM}" ] && [ ! -z "${GIT_SOURCE_XFRM}" ]; then
-		printf '%s\n' "${GIT_SOURCE_XFRM}"
+	if [ -n "$GIT_SOURCE_XFRM" ] && [ ! -z "$GIT_SOURCE_XFRM" ]; then
+		printf '%s\n' "$GIT_SOURCE_XFRM"
 	else
-		printf '%s\n' "${GIT_SOURCE_INFO}"
+		printf '%s\n' "$GIT_SOURCE_INFO"
 	fi
 }
 
@@ -56,16 +56,16 @@ get_utc_date()
 {
 	UTC_DATE=$(TZ=UTC date -u "+%D %T" 2> /dev/null)
 	# shellcheck disable=SC2236
-	if [ -n "${UTC_DATE}" ] && [ ! -z "${UTC_DATE}" ]; then
-		UTC_DATE_INFO=", ${RELENG} ${UTC_DATE}"
+	if [ -n "$UTC_DATE" ] && [ ! -z "$UTC_DATE" ]; then
+		UTC_DATE_INFO=", $RELENG $UTC_DATE"
 	else
 		UTC_DATE_INFO=""
 	fi
-	printf '%s\n' "${UTC_DATE_INFO}"
+	printf '%s\n' "$UTC_DATE_INFO"
 }
 
 BUILD_VER=$(get_git_info)
 BUILD_UTC=$(get_utc_date)
 
 printf '%s\n' "// Auto-generated git information." >verinfo.h
-printf '%s\n' "#define GIT_SOURCE_VERSION \"${BUILD_VER}${BUILD_UTC} (\"" >>verinfo.h
+printf '%s\n' "#define GIT_SOURCE_VERSION \"$BUILD_VER$BUILD_UTC (\"" >>verinfo.h
