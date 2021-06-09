264c8c6b4b505d6fcb69ed4edf111ecdc5de8adc
Restyled.io <commits@restyled.io>
2021-06-09 09:53:07 +0000

    Restyled by astyle

--- examples/example7.cpp
+++ examples/example7.cpp
@@ -1,14 +1,14 @@
 int main() {
-  int *pi;
-  int i;
+    int *pi;
+    int i;
 #ifdef DUMA_EXPLICIT_INIT
-  duma_init();
+    duma_init();
 #endif
-  pi = new int[10];
-  for (i = 0; i < 10; ++i)
-    pi[i] = i;
-  delete pi; // lgtm[cpp/new-array-delete-mismatch]
-  // above line should produce error, cause pi was allocated with new[]()
-  // unable to report allocation source - without including dumapp.h
-  return 0;
+    pi = new int[10];
+    for (i = 0; i < 10; ++i)
+        pi[i] = i;
+    delete pi; // lgtm[cpp/new-array-delete-mismatch]
+    // above line should produce error, cause pi was allocated with new[]()
+    // unable to report allocation source - without including dumapp.h
+    return 0;
 }
