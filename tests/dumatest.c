a290d824318d45e9a7d960a39d311a8d839e9b0b
Jeffrey H. Johnson <trnsz@pobox.com>
2021-06-13 21:21:42 -0400

    Update dates

--- tests/dumatest.c
+++ tests/dumatest.c
@@ -1,6 +1,7 @@
 /*
  * DUMA - Red-Zone memory allocator.
- * Copyright (C) 2002-2008 Hayati Ayguen <h_ayguen@web.de>, Procitec GmbH
+ * Copyright (C) 2020-2021 Jeffrey H. Johnson <trnsz@pobox.com>
+ * Copyright (C) 2002-2021 Hayati Ayguen <h_ayguen@web.de>, Procitec GmbH
  * Copyright (C) 1987-1999 Bruce Perens <bruce@perens.com>
  * License: GNU GPL (GNU General Public License, see COPYING-GPL)
  *
